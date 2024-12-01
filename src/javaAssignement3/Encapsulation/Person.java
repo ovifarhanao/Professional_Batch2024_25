9f20154b4ef5b595928100386a9c788350ef1864
ovi <ovifarhanao@gmail.com>
2024-12-01 18:16:01 +0600

    Assignment 3ww

---
+++ src/javaAssignement3/Encapsulation/Person.java
@@ -0,0 +1,19 @@
+package javaAssignement3.Encapsulation;
+
+public class Person {
+
+    //Private field
+    private String name;
+
+    //getter method for age
+    public String getName(){
+        return name;
+    }
+    //Setter method
+
+    public void setName(String name) {
+        this.name = name;
+        }
+    }
+
+
---
+++ src/javaAssignement3/InheritanceOOP/Animal.java
@@ -0,0 +1,9 @@
+package javaAssignement3.InheritanceOOP;
+
+
+//Base class/Super class/Parent class
+public class Animal {
+   public void eat() {
+        System.out.println("eating ...");
+    }
+}
