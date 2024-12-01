dc78d7c37d0a167385f1c2cd19ac51827e8135bd
ovi <ovifarhanao@gmail.com>
2024-12-01 18:42:53 +0600

    3rd

---
+++ src/javaAssignement4/nonGeneric/LongTest.java
@@ -0,0 +1,13 @@
+package javaAssignement4.nonGeneric;
+
+public class LongTest {
+
+     private Long aLong;
+
+    public LongTest(Long aLong) {
+        this.aLong = aLong;
+    }
+    public Long getLong() {
+        return aLong;
+    }
+}
