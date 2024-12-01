b9e59ef0b6dd0522336c797f472f7a3394f50cb9
ovi <ovifarhanao@gmail.com>
2024-12-01 18:42:53 +0600

    3rd

---
+++ src/javaAssignement4/nonGeneric/DoubleTest.java
@@ -0,0 +1,13 @@
+package javaAssignement4.nonGeneric;
+
+public class DoubleTest {
+    private Double aDouble;
+
+    public DoubleTest(Double aDouble) {
+        this.aDouble = aDouble;
+    }
+
+    public Double getaDouble() {
+        return aDouble;
+    }
+}
---
+++ src/javaAssignement4/nonGeneric/StringTest.java
@@ -0,0 +1,12 @@
+package javaAssignement4.nonGeneric;
+
+public class StringTest {
+
+    private  String str;
+    public StringTest(String str) {
+        this.str = str;
+    }
+    public String getString() {
+        return str;
+    }
+}
